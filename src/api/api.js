6afbc529923df0a192adec2eb2750db55c5a69ff
Daisy <kiennguyen12.com@gmail.com>
2025-03-26 20:58:19 +0700

    config url

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,13 @@
 import axios from 'axios';
 
+const BASE_URL_LOCAL = 'http://localhost:3030';
+const BASE_URL_PRODUCTION = 'http://103.155.161.81:3030';
+
 const api = axios.create({
-  baseURL: 'http://localhost:3030',
+  baseURL:
+    import.meta.env.MODE === 'production'
+      ? BASE_URL_PRODUCTION
+      : BASE_URL_LOCAL,
   withCredentials: true, // send cookies in every request
 });
 
